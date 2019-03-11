6c00788894516689d6068f2aad8e04557944fb5c
jprothwell <jprothwell@gmail.com>
2019-03-11 17:48:25 +0800

    .

--- iOSDropDown/Classes/iOSDropDown.swift
+++ iOSDropDown/Classes/iOSDropDown.swift
@@ -163,6 +163,9 @@ open class DropDown : UITextField{
         self.superview?.insertSubview(shadow, belowSubview: self)
         self.superview?.insertSubview(table, belowSubview: self)
         self.isSelected = true
+        
+        self.shadow.frame = self.table.frame
+        self.shadow.dropShadow()
         UIView.animate(withDuration: 0.3, animations: {
             self.table.frame = CGRect(x: self.frame.minX,
                                       y: self.frame.maxY+5,
@@ -170,8 +173,6 @@ open class DropDown : UITextField{
                                       height: self.tableheightX)
             
             self.table.alpha = 1
-            self.shadow.frame = self.table.frame
-            self.shadow.dropShadow()
             self.arrow.position = .up
         }) { (finish) in
             
@@ -183,6 +184,10 @@ open class DropDown : UITextField{
     public func hideList() {
         
         TableWillDisappearCompletion()
+        
+        self.shadow.alpha = 0
+        self.shadow.frame = self.table.frame
+        self.shadow.removeFromSuperview()
         UIView.animate(withDuration: 0.3, animations: {
             self.table.frame = CGRect(x: self.frame.minX,
                                       y: self.frame.maxY+5,
@@ -193,11 +198,8 @@ open class DropDown : UITextField{
                                       y: self.frame.minY,
                                       width: self.frame.width,
                                       height: 0)
-            self.shadow.alpha = 0
-            self.shadow.frame = self.table.frame
             self.arrow.position = .down
         }) { (finish) in
-            self.shadow.removeFromSuperview()
             self.table.removeFromSuperview()
             self.isSelected = false
             self.TableDidDisappearCompletion()
